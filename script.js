ecca082e4db3e9ddbd572a2dc286d1edc618a04b
AminuR Rasul RahuL <lidorara@gmail.com>
2021-10-04 18:35:41 -0400

    third commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var tenInput=$("#text10AM");
+var saveBtn=$("#save-btn");
+var textInput;
+
 //Selectors
 
 var timeDisplayEl = $('#time-display');
@@ -11,6 +15,34 @@ function displayTime() {
     timeDisplayEl.text(rightNow);
   }
 
+function btnFunction(){
+    console.log("print something");
+    textInput=tenInput.val();
+    console.log(textInput);
+    
+    localStorage.setItem('tenAM',textInput);
+  }
+tenInput.text(localStorage.getItem('tenAM'));
+
+
+// function planSubmit(event) {
+//     event.preventDefault();
+  
+//     var planName = planNameInputEl.val().trim();
+    
+//     var dueHour = dueHourInputEl.val().trim();
+  
+//     printPlanData(planName, dueHour);
+  
+//     planSubmitEl.reset();
+//   }
+
+
+
   //event listeners
 
+  saveBtn.on("click",btnFunction)
+
   setInterval(displayTime, 1000);
+
+  
